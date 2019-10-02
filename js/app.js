04aad7e36a9d4e9b11639458a6edb39af560ec20
ezziagheith <ezziagheith@gmail.com>
2019-10-02 16:08:53 -0700

    Added Timer

--- js/app.js
+++ js/app.js
@@ -1,50 +1,46 @@
 // console.log('Hey Kenny');
 
-// ANCHOR Add a listener to the begin button
+// ANCHOR Step One
+// Click start Button 
+// - add an event listener to click on button and function for timer to start
+// - function for timer to stop and to start again for round 2 and 3 with less time
+// images disappear when the time starts
 
-$("button").on('click', ()=>{
-console.log("Start Whacking!");
-setUpRound();
-setTimer();
+// ANCHOR Step Two
+// function? for images to begin sliding up/down randomly 
+// appear and disappear slower for round 1 
+// appear and disappear a little faster for round 2
+// appear and disappear faster for round 3
 
-})
+// ANCHOR Step Three
+// Player clicks on appearing images
+// function -- if player clicks on student, add a point
+// function -- if player clicks on instructor, minus a point
 
-// ANCHOR Set up a timer and a round.
+// ANCHOR Step Four
+// At the end of round 3, display score
 
-let time = 20;
-let round = 1;
 
-const setTimer = () => {
-    const timer = setInterval(() => {
-        console.log(time);
-        time --;
-        if(time===0){
-            clearInterval(timer);
-            round++;
-            setUpRound();
-            setTimer();
-        }
-        updateTime();
-    }, 1000)
+$("button").on("click", ()=>{
+    $('.mole').hide();
+    console.log("Start the game");
+    setTimer();
+  });
+
+  let time = 20;
+  
+  const setTimer = () => {
+  const timer = setInterval(()=>{
+      console.log(time);
+      time--;
+      if(time === 0){
+          clearInterval(timer);
+      } updateTime();
+  }, 1000)
 }
 
 const updateTime = () => {
-    const $timer = $('#timer');
-    $timer.text(`Timer: ${time}s`)
-}
+    const $timer = $("#timer");
+    $timer.text(`timer: ${time}s`);
+  }
 
-const setUpRound = () => {
-    $(".pics").empty();
-    $("#round").text(`Round: ${round}`);
-}
-
-const holes = $('.firstHoles')
-
-
-function randomTime(min, max) {
-    return Math.random() * (max-min) + min;
-}
-
-function randomHole(holes){
-    console.log(holes.length)
-}
